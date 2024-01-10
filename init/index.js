d52bbf2bda937bed76f7a2fb83f1553544408ec4
akshanshakki <akshanshakki22@gmail.com>
2024-01-10 19:21:59 +0530

    Add WanderLust Project

---
+++ init/index.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Listing = require("../models/listing.js");
+const initData = require("./data.js");
+
+// const mongo_url = "mongodb://127.0.0.1:27017/wanderlust"; //url for local storage
+const mongo_url = process.env.ATLASDB_URL;
+
+main()
+    .then(() => {
+        console.log("connected to db");
+    })
+    .catch((err) => {
+        console.log(`there's an error`);
+    });
+
+async function main() {
+    await mongoose.connect(mongo_url);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({
+        ...obj,
+        owner: "653fdab3f99728282fa8fbba",
+    }));
+    await Listing.insertMany(initData.data);
+    console.log("data initialized");
+};
+
+initDB();
